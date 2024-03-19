ffd9034dac986b36f90173226a77c7f3a3910030
Mayo Adebiyi <54384405+mayolab@users.noreply.github.com>
2024-03-19 19:15:46 -0400

    app.py

--- app.py
+++ app.py
@@ -7,14 +7,39 @@ app = Flask(__name__)
 
 model = pickle.load(open("fish-model.pkl", "rb"))
 
+@app.route('/', methods=['GET'])
+
+def home():
+    return render_template('index.html'),200
+
 
 @app.route('/predict', methods=['POST'])
 
 def predict():
 
+    if request.headers['Content-Type'] == 'application/json':
+        weight = request.get_json()['weight']
+        length1 = request.get_json()['length1']
+        length2 = request.get_json()['length2']
+        length3 = request.get_json()['length3']
+        height = request.get_json()['height']
+        width = request.get_json()['width']
+    else:
+        #question = request.form.get('input', '')
+
+        weight = request.form.get('weight', '')
+        length1 = request.form.get('length1', '')
+        length2 = request.form.get('length2','')
+        length3 = request.form.get('length3','')
+        height = request.form.get('height','')
+        width = request.form.get('width','')
+
+
+
+
     data = request.get_json()
 
-    df = pd.DataFrame(data)
+    df = [[weight, length1, length2, length3, height, width]]
 
     prediction = model.predict(df)
 
@@ -24,4 +49,3 @@ def predict():
 if __name__ == '__main__':
 
     app.run(debug=True)
-
